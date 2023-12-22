d9664d000aec4f6b165aa7dbdf8a511dc4bacea5
Stop <dbgusdn0922@naver.com>
2023-12-22 23:03:52 +0900

    add language module.

---
+++ language.js
@@ -0,0 +1,42 @@
+let currentLanguageId;
+
+const languageMap = new Map();
+
+function setLanguageData(languageId, data) {
+    languageMap.set(languageId, data);
+}
+
+function getLanguageData(languageId) {
+    return languageMap.get(languageId);
+}
+
+function selectLanguage(languageId) {
+    if (!languageMap.has(languageId)) {
+        throw 'Language not found';
+    }
+
+    currentLanguageId = languageId;
+}
+
+export function lang(strings, ...keys) {
+    const totalString = strings.slice(1).reduce((acc, cur, idx) => acc + cur + keys[idx], strings[0]);
+    const accessors = totalString.split('.');
+
+    let string = languageMap.get(currentLanguageId);
+
+    for (let i = 0; i < accessors.length; i++) {
+        if (!(accessors[i] in string)) {
+            throw `cannot find string data in ${currentLanguageId}: ${totalString}`;
+        }
+
+        string = string[accessors[i]];
+    }
+
+    return string;
+}
+
+export default {
+    setLanguageData,
+    getLanguageData,
+    selectLanguage,
+};
