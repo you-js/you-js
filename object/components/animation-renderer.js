c960b818d1093d336c5381b34434f14cea1ea6b8
Stop <dbgusdn0922@naver.com>
2024-03-14 04:58:29 +0900

    add animations and animation id to animation renderer.

--- object/components/animation-renderer.js
+++ object/components/animation-renderer.js
@@ -2,7 +2,9 @@ import { Component } from "../component.js";
 
 export class AnimationRenderer extends Component {
 
+    animations;
     animation;
+    animationId;
 
     onUpdate(deltaTime) {
         this.animation?.update(deltaTime);
@@ -10,5 +12,14 @@ export class AnimationRenderer extends Component {
 
     onRender(context) {
         this.animation?.render(context);
+    }
+
+    play(animationId) {
+        this.animation = this.animations[animationId];
+
+        if (this.animation == null) { return }
+
+        this.animationId = animationId;
+        this.animation.reset();
     }
 }
