357d881d474df121b7ed9244fcd7f3e3210b6797
Stop <dbgusdn0922@naver.com>
2024-03-28 02:05:41 +0900

    add constructor of sprite renderer. add opacity to sprite renderer.

--- object/components/sprite-renderer.js
+++ object/components/sprite-renderer.js
@@ -4,7 +4,25 @@ export class SpriteRenderer extends Component {
 
     sprite;
 
+    constructor({
+        sprite=null,
+        opacity=1,
+    }={}) {
+        super();
+
+        this.sprite = sprite;
+        this.opacity = opacity;
+    }
+
     onRender(context) {
-        this.sprite?.render(context);
+        if (this.sprite == null) { return }
+        if (this.opacity === 0) { return }
+
+        context.save();
+        context.globalAlpha = this.opacity;
+
+        this.sprite.render(context);
+
+        context.restore();
     }
 }
