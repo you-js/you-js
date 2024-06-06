f4aba7fd7f0ecc571ace125d8e270da9dcccf6ad
Stop <dbgusdn0922@naver.com>
2024-06-06 19:14:41 +0900

    modify stop function to terminate the system by platform type in youjs main module.

--- you.js
+++ you.js
@@ -1,11 +1,12 @@
-import {} from "./utility/array.js";
-import {} from "./utility/vector.js";
-import {} from "./utility/geometry.js";
-import {} from "./utility/math.js";
-import {} from "./utility/uuid.js";
-import { Screen } from "./framework/screen.js";
 import { ResourceParser } from "./asset/resource-parser.js";
 import { Core } from "./framework/core.js";
+import { Screen } from "./framework/screen.js";
+import platform from "./platform.js";
+import { } from "./utility/array.js";
+import { } from "./utility/geometry.js";
+import { } from "./utility/math.js";
+import { } from "./utility/uuid.js";
+import { } from "./utility/vector.js";
 
 let core = null;
 
@@ -30,6 +31,15 @@ function start(configurations) {
 function stop() {
     core.stop();
     core = null;
+
+    const platformId = platform.getId();
+
+    if (platformId === 'desktop') {
+        window.electronContextBridge.exit();
+    }
+    else if (platformId === 'web') {
+        window.location.reload();
+    }
 }
 
 export default { start, stop };
