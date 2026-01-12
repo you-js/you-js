b29fbb804f838f6418dae475700a32ea0e288c14
Youhyunwoo <dbgusdn0922@naver.com>
2026-01-12 18:20:40 +0900

    Configure project for npm library publishing

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export { Game, game } from './core/game.js';
+export { Entity } from './core/entity.js';
+export { Component } from './core/component.js';
+export { InputManager, input } from './core/input.js';
+export { Loader } from './core/loader.js';
+export { Sprite } from './core/sprite.js';
+export { SpriteRenderer } from './core/components/sprite-renderer.js';
+export { BoxCollider } from './core/components/box-collider.js';
+export { CollisionSystem } from './core/collision-system.js';
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   base: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'YouJS',
+      fileName: (format) => `you-js.${format}.js`
+    }
   }
 });
